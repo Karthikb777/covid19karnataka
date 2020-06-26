714072afd7d7a1b25f8b65a85a251b2b82f6deb4
Karthik Baraati <karthik.barati.777@gmail.com>
2020-06-26 19:56:43 +0530

    Updates

---
+++ precache-manifest.86161763b3f2e66b80ef732d8b8839d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d76a81a1e24dcf7ea0345c6580757e",
+    "url": "/covid19karnataka/index.html"
+  },
+  {
+    "revision": "70e242a1aa6526906cbc",
+    "url": "/covid19karnataka/static/css/main.f998c5cc.chunk.css"
+  },
+  {
+    "revision": "fbd3388d1472a79821df",
+    "url": "/covid19karnataka/static/js/2.d3a3d386.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/covid19karnataka/static/js/2.d3a3d386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e242a1aa6526906cbc",
+    "url": "/covid19karnataka/static/js/main.74a7e1f6.chunk.js"
+  },
+  {
+    "revision": "402ec47b0ceca275b00c",
+    "url": "/covid19karnataka/static/js/runtime-main.9c23398e.js"
+  }
+]);
